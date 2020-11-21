6c72968427118727b2dc9deb8087ef73feeffd6a
priya-abc <priyapriya6598@gmail.com>
2020-11-21 19:26:46 +0530

    added: Employee part time wage added

--- emp.sh
+++ emp.sh
@@ -24,3 +24,14 @@ for (( i=1; i<=30; i++))
 do
         if [[ ${attendance[$i]} = "Present" ]]
         then
+if [ ${partime[$i]} -eq 2 ]
+                then
+                        wage[$i]=`expr $Rate \* $Halftime`
+                else
+                        wage[$i]=`expr $Rate \* $Fulltime`
+                fi
+        else
+                wage[$i]=0
+fi
+                total_wage=`expr $total_wage + ${wage[$i]}`
+done
